ed932eb39843ce07f1e7c60fa384fdd31ccb2b1d
Wilfred Kotey <wilfred.kotey@gmail.com>
2023-05-18 09:20:59 -0700

    simple shell

--- shell.h
+++ shell.h
@@ -1,25 +1,194 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
-#define UNUSED(x) (void)(x)
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <limits.h>
-#include <signal.h>
 #include <fcntl.h>
+#include <errno.h>
 
-int _strlen(char *s);
-int digit(int c);
-int _strcmp(char *str1, char *str2);
-void simple_error(void);
-void print_error(char *argv, int cnt, char *cmd);
-void execute_error(char *argv, int cnt, char *temp_cmd);
-void print_str(char *str, int newline);
+/* for the buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
 
+/* this does command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+/* this is used for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/* system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
+
+
+/**
+ * struct liststr - for singly linked list
+ * @num: number field
+ * @str: string
+ * @next: points to next node in function
+ */
+
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ * struct passinf - has pseudo-arguments to pass into function
+ * which allows uniform prototype for function pointer struct
+ * @arg: string gotten from getline containing arguments
+ * @argv: array of strings gotten from arg
+ * @path: string path for current command
+ * @argc: argument count
+ * @err_count:  error count
+ * @err_num: error code for exit()s
+ * @linecount_flag: count this line of input
+ * @fname: program filename
+ * @env: linked list local copy of environ
+ * @environ: modified copy of environ from linked list env
+ * @hist: history node
+ * @alias: alias node
+ * @env_changed: if environ was changed
+ * @status: return status of last executed command
+ * @cmd_buf: address of pointer to cmd_buf
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @readfd: fd from which to read line input
+ * @histcount: history line number count
+ */
+typedef struct passinf
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int err_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *hist;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf;
+	int cmd_buf_type;
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+		0, 0, 0}
+
+/**
+ * struct builtin - builtin string and related function
+ * @type: builtin command flag
+ * @func: function
+ */
+
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
+
+
+int hsh(info_t *, char **);
+int find_builtin(info_t *);
+void find_cmd(info_t *);
+void fork_cmd(info_t *);
+int is_cmd(info_t *, char *);
+char *dup_chars(char *, int, int);
+char *find_path(info_t *, char *, char *);
+int loophsh(char **);
+void _eputs(char *);
+int _eputchar(char);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+char **strtow(char *, char *);
+char **strtow2(char *, char);
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
+int bfree(void **);
+int interactive(info_t *);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+int _erratoi(char *);
+void print_error(info_t *, char *);
+int print_d(int, int);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+int _myhistory(info_t *);
+int _myalias(info_t *);
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_vars(info_t *);
+int replace_string(char **, char *);
 
 #endif /* _SHELL_H_ header */
--- simple_functions.c
+++ simple_functions.c
@@ -1,93 +1,76 @@
 #include "shell.h"
 
 /**
- * _strcmp - this function compares two strings
- * @str1: 1st string
- * @str2: 2nd string
- * Return: 0 if strings match and -1 if they don't
+ * inter - true if shell is interactive
+ * @info: struct address
+ *
+ * Return: 1 for interactive mode and 0 if not
  */
 
-int _strcmp(char *str1, char *str2)
+int inter(info_t *info)
 {
-	int s;
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
 
-	if (_strlen(str1) != _strlen(str2))
-	{
-		return (-1);
-	}
-	for (s = 0; str1[s] != '\0'; s++)
-	{
-		if (str1[s] != str2[s])
-		{
-			return (-1);
-		}
-	}
+/**
+ * is_delim - checks for delimeted characters
+ * @c: character to check
+ * @delim: delimeter string
+ * Return: 1 for successful and 0 otherwise
+ */
+
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
 	return (0);
 }
 
 /**
- * simple_error - function prints simple error
- * Return: void
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
 
-void simple_error(void)
+int _isalpha(int c)
 {
-	perror("Fatal Error");
-	exit(100);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
 
 /**
- * print_error - function that prints error message if command not found
- * @argv: name of shell program
- * @cnt: number of commands running on shell
- * @cmd: command not found
- * Return: void
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
  */
 
-void print_error(char *argv, int cnt, char *cmd)
+int _atoi(char *s)
 {
-	print_str(argv, 1);
-	print_str(": ", 1);
-	print_number(cnt);
-	print_str(": ", 1);
-	print_str(cmd, 1);
-}
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-/**
- * print_str - function prints a string character by character
- * @str: string to print
- * @newline: if the interger is zero a newline is printed
- * Return: void
- */
-
-void print_str(char *str, int newline)
-{
-	int a;
-
-	if (str == NULL)
-		str = "(null)";
-	for (a = 0; str[a] != '\0'; a++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		write(STDOUT_FILENO, &str[a], 1);
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
 	}
 
-	if (newline == 0)
-	{
-		write(STDOUT_FILENO, "\n", 1);
-	}
-}
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
 
-/**
- * execute_error - a function that prints execution errors
- * @argv: the name of the shell program
- * @cnt: the number of commands running on the shell
- * @temp_cmd: the command that fails
- */
-
-void execute_error(char *argv, int cnt, char *temp_cmd)
-{
-	print_error(argv, cnt, temp_cmd);
-	print_str(": ", 1);
-	perror("");
-	exit(1);
+	return (output);
 }
